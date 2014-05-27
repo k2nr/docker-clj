585633c8a9a6ab6b7377dd89ca7f767c7d52f41f
Kazunori Kajihiro <likerichie@gmail.com>
2014-05-28 02:49:18 +0900

    prepare for 0.0.3 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject k2nr/docker "0.0.2"
+(defproject k2nr/docker "0.0.3-SNAPSHOT"
   :description "Docker Remote API client library for Clojure"
   :url "https://github.com/k2nr/docker-clj"
   :license {:name "Eclipse Public License"
